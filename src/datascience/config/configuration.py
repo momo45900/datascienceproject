9c1abc5aa7c2aea13734486a9e17f0fe756fedd1
momo45900 <slah45900@gmail.com>
2024-10-28 14:24:30 +0400

    model evaluation

--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -88,8 +88,7 @@ class ConfigurationManager:
             all_params=params,
             metric_file_name = config.metric_file_name,
             target_column = schema.name,
-            mlflow_uri="https://dagshub.com/krishnaik06/datascienceproject.mlflow"
-
+            mlflow_uri="https://dagshub.com/slah45900/datascienceproject.mlflow"
 
         )
         return model_evaluation_config
